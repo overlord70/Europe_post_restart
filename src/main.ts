2e6a677760020b1dd535651261da97d4f2f0b66f
overlord70 <s37776270@gmail.com>
2024-09-01 19:01:03 +0500

    100%

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import {create_header, auto_reload} from "../modules/ui"
 import { Background_code } from "./types";
 
 const http = new MakeRequest()
-
 const flex_ads = document.querySelector('.flex_ads')as HTMLElement
-
 create_header()
 
 auto_reload(flex_ads, Background_code)
